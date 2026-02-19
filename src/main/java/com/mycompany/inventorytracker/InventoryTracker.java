f2d336fa54f6ee0b7ff1ea63d4cbecb7be216954
LorenzoSJ <lorenzomsanjuan@gmail.com>
2026-02-19 17:02:53 -0500

    Project Setup By Lorenzo San Juan

---
+++ src/main/java/com/mycompany/inventorytracker/InventoryTracker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventorytracker;
+
+/**
+ *
+ * @author LorenzoSJ
+ */
+public class InventoryTracker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
